efe5dc5a2d97bcc1b23e1a7f33afc95bd89fb831
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2019-10-21 12:25:31 -0400

    tested the app and logged initial and updated states

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-console.log('From index.js')
 //Actions
+
+const redux = require('redux') //using redux
+const createStore = redux.createStore //creating a store with redux
 const BUY_CAKE = 'BUY_CAKE'
 
 
-function buyCake() {
+function buyCake() { //action creator
     return {
     type: BUY_CAKE,
     info: 'First redux action'
@@ -23,3 +25,11 @@ const reducer = (state = InitialState, action) => {
         default: return state
     }
 }
+
+const store = createStore(reducer); //creating a redux store holding the app's initial state with the method
+console.log('Initial state', store.getState()); //gives an initial state of the app
+const unsubscribe = store.subscribe(() => console.log('updated store', store.getState())) //to allow app to subscribe to changes in the store
+store.dispatch(buyCake())//dispatch an action
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+unsubscribe();//unsubscribe from the store
